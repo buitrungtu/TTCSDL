dfdc2920448201f21f136fef37b2048bd6088f8b
tta9799zzs <tta9799@gmail.com>
2020-06-28 23:05:14 +0700

    Thêm form Login

--- QuanLyThuVien/Form_Login/fLogin.Designer.cs
+++ QuanLyThuVien/Form_Login/fLogin.Designer.cs
@@ -75,6 +75,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(311, 165);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 43);
@@ -85,6 +86,7 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(106, 165);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(137, 43);
